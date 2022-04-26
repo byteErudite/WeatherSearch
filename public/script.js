fb21d23f49b73b38e0b9a6a4243c5ef574d66b1c
byteErudite <raghuvanshivibhu@gmail.com>
2022-04-26 19:28:02 +0530

    Added changes in script.js

--- public/script.js
+++ public/script.js
@@ -20,3 +20,21 @@ searchBox.addListener('places_changed', () => {
   })
 })
 
+const icon = new Skycons({ color: '#222' })
+const locationElement = document.querySelector('[data-location]')
+const statusElement = document.querySelector('[data-status]')
+const temperatureElement = document.querySelector('[data-temperature]')
+const precipitationElement = document.querySelector('[data-precipitation]')
+const windElement = document.querySelector('[data-wind]')
+icon.set('icon', 'clear-day')
+icon.play()
+
+function setWeatherData(data, place) {
+  locationElement.textContent = place
+  statusElement.textContent = data.summary
+  temperatureElement.textContent = data.temperature
+  precipitationElement.textContent = `${data.precipProbability * 100}%`
+  windElement.textContent = data.windSpeed
+  icon.set('icon', data.icon)
+  icon.play()
+}
